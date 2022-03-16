01859bc899bbd8d456146ce78d80d983f2de9e52
james <james.jomuad@outlook.com>
2022-03-16 20:39:11 +0800

    Added vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,13 +20,17 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('Todo', require('./components/Todo.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify);
 const app = new Vue({
+    vuetify : new Vuetify(),
     el: '#app',
 });
